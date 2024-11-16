5e81f793c90b7db4756471cf046c3f9a3bed0843
Uwe Draisbach <100274932+uwedr@users.noreply.github.com>
2024-11-16 18:14:37 +0100

    Update main.js

--- main.js
+++ main.js
@@ -69,7 +69,9 @@
 		   break;
 	      default:
 		    // ????
+		break;
 		};
+	  }
 		
 		try {
 			const response = await fetch('https://f1dappl0.test.sozvers.at:44320/sap/opu/odata4/sap/zapi_bc_sac_bp_request_o4/srvd_a2x/sap/zbc_sac_bp_request/0001/BPRequest?sap-client=612', {
