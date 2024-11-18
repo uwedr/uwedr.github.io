f2765e6e4780901f5441cd608d56378b481f5a68
uwedr <uwe.draisbach@gmail.com>
2024-11-18 11:34:50 +0100

    update

--- main.js
+++ main.js
@@ -119,7 +119,7 @@ const DEFAULT_SAPCLIENT = '612';
 			} catch(error) {
 				console.log('Fehler in Methode createProjectWithWBS.');
 				result.result = 'Exception';
-				result.messages = [ 'CSRF-Token konnte nicht ermittelt werden.' ];
+				result.messages = [ error.stack ];
 				console.log(result.message);
 				return result;
 			}
