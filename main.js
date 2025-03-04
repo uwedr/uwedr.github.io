78e5ac9f2bb9965d729e71dfae6c80a853bd7137
uwedr <uwe.draisbach@gmail.com>
2025-03-04 01:18:59 +0100

    update

--- main.js
+++ main.js
@@ -227,7 +227,7 @@ const ACCESS_CONTROL_ALLOW_ORIGIN = 'https://gesundheitskasse-q.eu20.analytics.c
 					res = await response.json();
 					result.messages = (await res.error.message === "") ? [] : [res.error.message];
 					if (res.error.hasOwnProperty('details')) {
-						let msg = (await res.error.details.map((x) => x.message);
+						let msg = (await res.error.details.map(x => x.message));
 						result.messages.concat(msg);				
 					};
 					break;
